9dcbe63e33c693bc0737f410bc4883d21a8f34f6
kupre1 <160654812+kupre1@users.noreply.github.com>
2024-11-08 20:00:11 +0400

    chnage
    chaneg

---
+++ app/constants/card.ts
@@ -0,0 +1,14 @@
+export type CardType = {
+  title: string;
+  id: number;
+  image: string;
+};
+
+export const cards: CardType[] = [
+  { title: "title 1", id: 1, image: "./assets/images/image1.png" },
+  { title: "title 2", id: 2, image: "./assets/images/image2.png" },
+  { title: "title 3", id: 3, image: "./assets/images/image3.png" },
+  { title: "title 4", id: 4, image: "./assets/images/image4.png" },
+  { title: "title 5", id: 5, image: "./assets/images/image5.png" },
+  { title: "title 6", id: 6, image: "./assets/images/image6.png" },
+];
